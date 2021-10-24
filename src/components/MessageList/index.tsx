4b4c3ba3d2bb0f93fb67d4ba91c777ce627567e9
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-24 05:52:27 -0300

    Agora as mensagens estão vindo do bando de dados do prisma

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,16 +1,28 @@
-import styles from './styles.module.scss'
-
 import {api} from '../../service/api'
 import Logox from '../../assets/logo.svg'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import styles from './styles.module.scss'
+
+type Message = {
+
+    id: string;
+    text: string;
+    user: {
+       name: string;
+       avatar_url: string;
+    }
+}
+
 export function MessageList(){
 
+    const [messages, setMessages] = useState<Message[]>([])
+
     useEffect(() => {
-        api.get('messages/last3').then(response => {
-            console.log(response.data);
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data)
+            // console.log(response.data);
         })
     }, [] )
-
 
  return(
 //    Container geral
@@ -19,45 +31,20 @@ export function MessageList(){
        <img src={Logox} alt="Logo" />
    
    <ul className={styles.messageList}>
-         <li className={styles.message}> 
-             <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos</p>
+       {messages.map(message => {
+           return(
+                 <li className={styles.message}> 
+             <p className={styles.messageContent}>{message.text}</p>
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
-                    <img src="https://github.com/reginaldo007oliveira.png" alt="Olá mundo" />
-
+                    <img src={message.user.avatar_url} alt={message.user.name} />
                 </div>
-                  <span>Reginaldo Oliveira</span>
+                  <span>{message.user.name}</span>
               </div>
          </li>
-
-         <li className={styles.message}> 
-             <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos</p>
-              <div className={styles.messageUser}>
-                <div className={styles.userImage}>
-                    <img src="https://github.com/reginaldo007oliveira.png" alt="Olá mundo" />
-
-                </div>
-                  <span>Reginaldo Oliveira</span>
-              </div>
-         </li>
-
-         <li className={styles.message}> 
-             <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos</p>
-              <div className={styles.messageUser}>
-                <div className={styles.userImage}>
-                    <img src="https://github.com/reginaldo007oliveira.png" alt="Olá mundo" />
-
-                </div>
-                  <span>Reginaldo Oliveira</span>
-              </div>
-         </li>
+           );    
+       })}     
    </ul>
-   
-   
-   
    </div>
  )
-
-  
- 
 }
