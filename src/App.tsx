083abf5c3e26621a7d877e45beaa9dbaa457a266
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-25 16:47:57 -0300

    Message List Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 // Quando exportamos o componente na função temos que passar ela com js no main {app}
 //quando exportamos por default não precisa colocar as {} na hora da importação.
+import { useContext } from 'react';
 import  styles from './App.module.scss';
 import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './context/auth';
 export function App() {
+     const { user} = useContext(AuthContext);
 
   return (
     <main className={styles.contentWrapper}>
       <h1><MessageList /></h1>
+      {!! user ? <SendMessageForm /> : <LoginBox />}
      <h1><LoginBox /></h1> 
       
     </main>
