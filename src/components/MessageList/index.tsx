c20b76f1e2904d3a67b5c0b4fb87f6f04a61f687
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-25 18:28:48 -0300

    Web socket Ok .. transição de mensagens

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@ import {api} from '../../service/api'
 import Logox from '../../assets/logo.svg'
 import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
+import io from 'socket.io-client'
 
 type Message = {
 
@@ -13,9 +14,32 @@ type Message = {
     }
 }
 
+let messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
+
 export function MessageList(){
 
     const [messages, setMessages] = useState<Message[]>([])
+    //insere no banco e mostra na tela em 3s
+    useEffect(() => {
+        setInterval(() => {
+          if (messagesQueue.length > 0) {
+            setMessages(prevState => [
+              messagesQueue[0],
+              prevState[0],
+              prevState[1],
+            ].filter(Boolean))
+    
+            messagesQueue.shift()
+          }
+        }, 3000)
+      }, [])
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response => {
